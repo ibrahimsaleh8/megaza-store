fdd02998945e49862539c66034330e35ea56be83
Noyan <ebrihm576@gmail.com>
2025-03-04 14:13:42 +0200

    Final Update

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,15 +1,12 @@
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export function GET() {
   try {
+    cookies().delete("token");
     return NextResponse.json(
-      { message: "Logged out successfully" },
-      {
-        status: 200,
-        headers: {
-          "Set-Cookie": `token=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT; HttpOnly; SameSite=Lax`,
-        },
-      }
+      { message: "Logeed out Success" },
+      { status: 200 }
     );
   } catch (error) {
     return NextResponse.json({ message: "Error " + error }, { status: 500 });
